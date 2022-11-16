0b82d79f816f3a99dc758581affd23d1243247e3
M V K <krishna.mugala9@gmail.com>
2022-11-16 12:33:47 +0530

    created a beans in the config class

--- src/main/java/domain/ProductConfig.java
+++ src/main/java/domain/ProductConfig.java
@@ -16,4 +16,12 @@ public class ProductConfig {
     public Product getProductBean1() {
         return new Product(1, "macbook air", 120000, "apple product", "excellent");
     }
+    @Bean("product2")
+    public Product getProductBean2() {
+        return new Product(2, "rolex", 300000, "rolex product", "excellent");
+    }
+    @Bean("product3")
+    public Product getProductBean3() {
+        return new Product(3, "nike ", 12000, "nike product", "good");
+    }
 }
