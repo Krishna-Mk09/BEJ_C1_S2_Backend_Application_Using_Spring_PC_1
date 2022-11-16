a875da0d96d4c8060bf007bd302585029370654a
M V K <krishna.mugala9@gmail.com>
2022-11-16 12:39:59 +0530

    called the beans and printed on the console using to string method

--- src/main/java/com/niit/jdp/App.java
+++ src/main/java/com/niit/jdp/App.java
@@ -1,13 +1,18 @@
 package com.niit.jdp;
 
+import domain.Product;
+import domain.ProductConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProductConfig.class);
+        Product product1 = (Product) context.getBean("product1");
+        System.out.println(product1);
+
     }
 }
