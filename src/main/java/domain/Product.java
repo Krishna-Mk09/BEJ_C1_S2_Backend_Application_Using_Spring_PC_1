e9e9337319b80320dd359ea48e216291d4b81b7f
M V K <krishna.mugala9@gmail.com>
2022-11-16 12:24:10 +0530

    created getter and setter methods in product class

--- src/main/java/domain/Product.java
+++ src/main/java/domain/Product.java
@@ -25,4 +25,44 @@ public class Product {
         this.description = description;
         this.productRating = productRating;
     }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProductRating() {
+        return productRating;
+    }
+
+    public void setProductRating(String productRating) {
+        this.productRating = productRating;
+    }
 }
